675d3fce3b3b96855bcce80149265c743fb5e2b9
Aditya Chauhan <caditya2811@gmail.com>
2026-09-21 20:07:41 +0530

    done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,9 +8,9 @@ const Main = () => {
         <li key={ingredient}>{ingredient}</li>
     ))
 
-    function handleSubmit(event) {
-        event.preventDefault()
-        const formData = new FormData(event.currentTarget)
+    function addIngredient(formData) {
+        
+        
         const newIngredient = formData.get("ingredient")
         setIngredients(prevIngredients => [...prevIngredients, newIngredient])
     }
@@ -20,7 +20,7 @@ const Main = () => {
 
   return (
    <main>
-    <form  onSubmit={handleSubmit} className='add-ing-form'>
+    <form  action={addIngredient} className='add-ing-form'>
         <input
         type='text'
         placeholder='e.g. oregano'
@@ -29,9 +29,18 @@ const Main = () => {
          />
         <button>Add ingredient</button>
     </form>
-    <ul>
-                {ingredientsListItems}
-            </ul>
+    {ingredients.length > 0 && <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+                {ingredients.length > 3 && <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button>Get a recipe</button>
+                </div>}
+            </section>}
+
    </main>
   )
 }
